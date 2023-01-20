5e6c6934e28d0bb59fe2a2990ff7eb46aedc352f
deluxc <56850167+deluxc@users.noreply.github.com>
2023-01-20 10:36:16 +0200

    Update Program.cs

--- 2.SumMatrixColumns/Program.cs
+++ 2.SumMatrixColumns/Program.cs
@@ -1,35 +1,41 @@
 ﻿using System;
 using System.Linq;
 
-namespace _1.SumMatrixElements
+namespace _2.SumMatrixColumns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] array = Console.ReadLine()
-                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                .Split(",", StringSplitOptions.RemoveEmptyEntries);
             var rows = int.Parse(array[0]);
             var cols = int.Parse(array[1]);
 
             var matrix = new int[rows, cols];
-            var sum = 0;
 
             for (int row = 0; row < rows; row++)
             {
                 int[] data = Console.ReadLine()
-                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse)
                     .ToArray();
+
                 for (int col = 0; col < cols; col++)
                 {
                     matrix[row, col] = data[col];
-                    sum += matrix[row, col];
                 }
             }
-            Console.WriteLine(rows);
-            Console.WriteLine(cols);
-            Console.WriteLine(sum);
+
+            for (int col = 0; col < cols; col++)
+            {
+                var sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
         }
     }
 }
