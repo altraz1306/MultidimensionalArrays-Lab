693165cb4bd3d7dafb9188af94ea6941a96a18be
deluxc <56850167+deluxc@users.noreply.github.com>
2023-01-20 10:36:16 +0200

    Update Program.cs

--- 1.SumMatrixElements/Program.cs
+++ 1.SumMatrixElements/Program.cs
@@ -30,20 +30,6 @@ namespace _1.SumMatrixElements
             Console.WriteLine(rows);
             Console.WriteLine(cols);
             Console.WriteLine(sum);
-
-            //PrintMatrix(matrix);
-        }
-
-        public static void PrintMatrix(int[,] matrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    Console.WriteLine($"{matrix[row, col]} ");
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
